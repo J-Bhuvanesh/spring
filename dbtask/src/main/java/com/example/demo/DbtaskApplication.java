1e7848d8cb2d38c929f5fff8efde9792fd99cac5
bhuvanesh <jbhuvaneshkumar1998@gmail.com>
2021-04-28 12:22:54 +0530

    first commit

---
+++ dbtask/src/main/java/com/example/demo/DbtaskApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DbtaskApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbtaskApplication.class, args);
+		System.out.println("SERVER STARTED lSUCCESSFULL.....");
+	}
+
+
+
+}
